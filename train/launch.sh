a21a18ea3f4df29f251928fdc76d856a7c58ddb4
leandermaerkisch <l.maerkisch@gmail.com>
2025-03-16 21:38:07 -0700

    Update training launch script and file paths for model training

--- train/launch.sh
+++ train/launch.sh
@@ -3,11 +3,11 @@
 uid="$(date +%Y%m%d-%H%M%S)"
 
 srun --job-name=XXX \
-    --nodes=2 \
+    --nodes=1 \
     --ntasks-per-node=1 \
     --account=YYY \
     --partition=ZZZ \
-    --gpus-per-task=8 \
+    --gpus-per-task=2 \
     --mem=256G \
     --time=3-00:00:00 \
     ./train/sft_slurm.sh \
