39fe61388260f4e22ee406a5e74474b844425068
leandermaerkisch <l.maerkisch@gmail.com>
2025-03-16 21:45:34 -0700

    fix joining

--- train/sft.sh
+++ train/sft.sh
@@ -35,7 +35,7 @@ torchrun --nproc-per-node ${gpu_count} --master_port 12345 \
     --adam_beta2=0.95 \
     --output_dir="ckpts/m1-${uid}" \
     --push_to_hub=${push_to_hub} \
-    --save_only_model=True
+    --save_only_model=True \
     --gradient_checkpointing=True # \ Enable gradient checkpointing for efficient memory usage with 8 H100 GPUs.
     # --accelerator_config='{"gradient_accumulation_kwargs": {"sync_each_batch": true}}'
 
